2d776f12b1f2d14ccf48bfbdaf4e5c55e694a2d2
rtoddf <rtoddf@me.com>
2026-05-13 16:00:46 -0400

    got cards on the page and mostly styled-todd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 // import { useState } from 'react';
 import './styles/colors-and-type.css';
+import './styles/card.css';
+import zaman from '/assets/characters/zaman.png';
+import aella from '/assets/characters/aella.png';
+import azrael from '/assets/characters/azrael.png';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -7,50 +11,50 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="grid grid-cols-4 gap-4">
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae
-            ullamcorper ipsum. Mauris condimentum hendrerit ex, vel blandit
-            tortor. Donec tristique lorem ac leo lacinia iaculis. Fusce
-            elementum dapibus odio vitae aliquet. Mauris et faucibus nunc, ut
-            cursus massa. Cras ac tincidunt turpis, ac blandit est. Quisque
-            vitae hendrerit eros. Cras cursus felis velit, id tincidunt nulla
-            porttitor et. Sed lacinia dapibus purus dignissim dapibus.
-            Vestibulum congue magna in neque vulputate, eget dignissim mi
-            dictum.
+        <div className="grid grid-cols-3 gap-4">
+          <div className="card">
+            <div className="portrait p-8">
+              <img src={zaman} alt="Zaman" />
+            </div>
+            <div className="body">
+              <h3 className="name">Zaman</h3>
+              <span className="tag">Time</span>
+              <p className="bio">
+                There once was a girl with deep pockets, Full of acorns and
+                string and old lockets. She'd count them by name, Each evening
+                the same, Then sleep with them tucked in their sockets.
+              </p>
+            </div>
           </div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae
-            ullamcorper ipsum. Mauris condimentum hendrerit ex, vel blandit
-            tortor. Donec tristique lorem ac leo lacinia iaculis. Fusce
-            elementum dapibus odio vitae aliquet. Mauris et faucibus nunc, ut
-            cursus massa. Cras ac tincidunt turpis, ac blandit est. Quisque
-            vitae hendrerit eros. Cras cursus felis velit, id tincidunt nulla
-            porttitor et. Sed lacinia dapibus purus dignissim dapibus.
-            Vestibulum congue magna in neque vulputate, eget dignissim mi
-            dictum.
+
+          <div className="card">
+            <div className="portrait p-8">
+              <img src={aella} alt="Aella" />
+            </div>
+            <div className="body">
+              <h3 className="name">Aella</h3>
+              <span className="tag">Mischief</span>
+              <p className="bio">
+                There once was a girl with deep pockets, Full of acorns and
+                string and old lockets. She'd count them by name, Each evening
+                the same, Then sleep with them tucked in their sockets.
+              </p>
+            </div>
           </div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae
-            ullamcorper ipsum. Mauris condimentum hendrerit ex, vel blandit
-            tortor. Donec tristique lorem ac leo lacinia iaculis. Fusce
-            elementum dapibus odio vitae aliquet. Mauris et faucibus nunc, ut
-            cursus massa. Cras ac tincidunt turpis, ac blandit est. Quisque
-            vitae hendrerit eros. Cras cursus felis velit, id tincidunt nulla
-            porttitor et. Sed lacinia dapibus purus dignissim dapibus.
-            Vestibulum congue magna in neque vulputate, eget dignissim mi
-            dictum.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae
-            ullamcorper ipsum. Mauris condimentum hendrerit ex, vel blandit
-            tortor. Donec tristique lorem ac leo lacinia iaculis. Fusce
-            elementum dapibus odio vitae aliquet. Mauris et faucibus nunc, ut
-            cursus massa. Cras ac tincidunt turpis, ac blandit est. Quisque
-            vitae hendrerit eros. Cras cursus felis velit, id tincidunt nulla
-            porttitor et. Sed lacinia dapibus purus dignissim dapibus.
-            Vestibulum congue magna in neque vulputate, eget dignissim mi
-            dictum.
+
+          <div className="card">
+            <div className="portrait p-8">
+              <img src={azrael} alt="Azrael" />
+            </div>
+            <div className="body">
+              <h3 className="name">Azrael</h3>
+              <span className="tag">Death</span>
+              <p className="bio">
+                There once was a girl with deep pockets, Full of acorns and
+                string and old lockets. She'd count them by name, Each evening
+                the same, Then sleep with them tucked in their sockets.
+              </p>
+            </div>
           </div>
         </div>
         {/* <button
