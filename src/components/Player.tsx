019aa30b0855e761baa2f795baf7fafd5859d822
rtoddf <rtoddf@me.com>
2026-05-27 16:13:51 -0400

    added first mp3 and simple audio player html-todd

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,7 +1,12 @@
+import mySound from '/assets/music/intangimere.mp3'; // Import local file
 import '../styles/colors-and-type.scss';
 
 function Player() {
-  return <p>player</p>;
+  return (
+    <div>
+      <audio src={mySound} controls />
+    </div>
+  );
 }
 
 export default Player;
