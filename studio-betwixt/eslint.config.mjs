659f45ad40125c060d90ba21c43ca25d7998845d
rtoddf <rtoddf@me.com>
2026-05-29 13:43:12 -0400

    es lint error-todd

--- studio-betwixt/eslint.config.mjs
+++ studio-betwixt/eslint.config.mjs
@@ -1,3 +1,10 @@
+export default [
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  // ... rest of your config below
+]
+
 import studio from '@sanity/eslint-config-studio'
 
 export default [...studio]
