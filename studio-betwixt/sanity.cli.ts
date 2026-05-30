c3fcebda7cf11ddd2a0f12c848fb451a948a4eac
rtoddf <rtoddf@me.com>
2026-05-30 11:00:55 -0400

    deployed admin-todd

--- studio-betwixt/sanity.cli.ts
+++ studio-betwixt/sanity.cli.ts
@@ -6,10 +6,7 @@ export default defineCliConfig({
     dataset: 'pr'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
     autoUpdates: true,
+    appId: 'wvugxgt2oabs9y6xydvtobob',
   }
 })
