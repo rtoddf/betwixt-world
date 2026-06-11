35fdcf9cad114be69c1ac2e5f40636f56b86859e
rtoddf <rtoddf@me.com>
2026-06-11 12:30:30 -0400

    set up about page with intent to do grid layout in sanity-todd

--- src/components/audio/Player.tsx
+++ src/components/audio/Player.tsx
@@ -32,7 +32,7 @@ function Player({
   const [duration, setDuration] = useState<number>(0);
   const [isMuted, setIsMuted] = useState<boolean>(false);
   const [showCC, setShowCC] = useState<boolean>(false);
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  // const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -41,18 +41,18 @@ function Player({
     const updateTime = () => setCurrentTime(audio.currentTime);
     const updateDuration = () => setDuration(audio.duration || 0);
     const handleEnded = () => setIsPlaying(false);
-    const handleCanPlay = () => setIsLoaded(true);
+    // const handleCanPlay = () => setIsLoaded(true);
 
     audio.addEventListener('timeupdate', updateTime);
     audio.addEventListener('loadedmetadata', updateDuration);
     audio.addEventListener('ended', handleEnded);
-    audio.addEventListener('canplay', handleCanPlay);
+    // audio.addEventListener('canplay', handleCanPlay);
 
     return () => {
       audio.removeEventListener('timeupdate', updateTime);
       audio.removeEventListener('loadedmetadata', updateDuration);
       audio.removeEventListener('ended', handleEnded);
-      audio.removeEventListener('canplay', handleCanPlay);
+      // audio.removeEventListener('canplay', handleCanPlay);
     };
   }, []);
 
