2386a7929118de8ec04f9dbea5db09146f7f633d
rtoddf <rtoddf@me.com>
2026-07-11 15:32:36 -0400

    added test map-todd

--- schemaTypes/hoodMap.ts
+++ schemaTypes/hoodMap.ts
@@ -41,15 +41,51 @@ export const hoodMap = defineType({
         },
       },
     }),
+    defineField({
+      name: 'testMapImage',
+      title: 'Test Map Image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'testHoodMarkers',
+      title: 'Test Hood Markers',
+      type: 'array',
+      of: [
+        {
+          name: 'hoodMarker',
+          type: 'object',
+          fields: [
+            {name: 'x', type: 'number', readOnly: true, initialValue: 50},
+            {name: 'y', type: 'number', readOnly: true, initialValue: 50},
+            {
+              name: 'neighborhood',
+              type: 'reference',
+              to: [{type: 'neighborhood'}],
+              options: {
+                filter: 'date <= now()',
+              },
+
+              validation: (Rule) => Rule.required(),
+            },
+          ],
+        },
+      ],
+      options: {
+        imageHotspot: {
+          imagePath: 'testMapImage',
+        },
+      },
+    }),
   ],
   preview: {
     select: {
       media: 'mapImage',
+      testMedia: 'testMapImage',
     },
-    prepare({media}) {
+    prepare({media, testMedia}) {
       return {
         title: 'Hood Map',
-        media,
+        media: testMedia || media,
       }
     },
   },
